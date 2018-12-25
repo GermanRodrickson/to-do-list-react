6de06d4f2c79c168dc88a3fb13ab785e4d45a60a
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-25 12:43:56 +0100

    box

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ const Wrapper = styled.div`
   background: #f4f4f4;
   width: 100%;
   height: 100vh;
-
+  position: relative;
+`
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  margin-top: -150px;
+  margin-right: -150px;
+  background: #ffffff;
 `
 
 class App extends Component {
   render() {
     return (
-      <Wrapper>Hello</Wrapper>
+      <Wrapper>
+        <Box>
+          Hello
+        </Box>
+      </Wrapper>
     );
   }
 }
