a38a6c2dcfe9de3ce2450344413203f8b63a6bfe
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-25 12:56:30 +0100

    css form

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+
+const Wrapper = styled.div`
+`
+
 const InputText = styled.input`
   box-shadow: none;
   border: 1px solid black;
-  width: 99%;
+  flex: 1;
+  width: 70%;
+  margin-left: 5px;
 `;
+const Button = styled.input`
+  width: 23%;
+  background-color: #f4f4f4;
+  margin-right: 5px;
+`;
+
+
 
 class Input extends Component {
   render() {
     return (
-      <InputText type="text" />
+      <Wrapper>
+        <InputText type="text" /> <Button type="submit" value="Add"/>
+      </Wrapper>
     );
   }
 }
